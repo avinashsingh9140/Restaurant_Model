49c7ff8394f9388a376dab5565e22ce872b9584c
Avinash <jaikisingh302@gmail.com>
2023-09-17 19:19:10 +0530

    Restaurant

---
+++ src/main/java/com/geekster/weeklyTest/project/RestaurantManagementApplication/RestaurantApplicationAPI/Model/Type.java
@@ -0,0 +1,5 @@
+package com.geekster.weeklyTest.project.RestaurantManagementApplication.RestaurantApplicationAPI.Model;
+
+public enum Type {
+    FAMILY, CAFE, DINNER, FAST_FOOD, BAR, BUFFET, OUTLET, FOOD_PARLOUR, CAKE_OUTLET, ICECREAM_PARLOUR
+}
